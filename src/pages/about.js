750cbdedfb5e8cb57c12e59956689e22b1ddb8ab
Danli Denny Deng <danlideng@yahoo.com>
2022-01-28 23:30:37 -0800

    index.js and about.js

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+// Step 1: Import React
+import * as React from 'react'
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Danli Denny Deng</title>
+      <h1>About Me</h1>
+      <p>Hi there! Denny is the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+// Step 3: Export your component
+export default AboutPage
